c1fab8ad2760effea2aaef6dc21e74325370a027
AZn5ReD <AZn5ReD@users.noreply.github.com>
2019-08-04 02:46:14 +0200

    Authenticate OK

--- src/index.js
+++ src/index.js
@@ -1,43 +1,33 @@
 const {
   BaseKonnector,
   requestFactory,
-  signin,
   scrape,
   saveBills,
   log,
   utils
 } = require('cozy-konnector-libs')
 const request = requestFactory({
-  // The debug mode shows all the details about HTTP requests and responses. Very useful for
-  // debugging but very verbose. This is why it is commented out by default
-  // debug: true,
-  // Activates [cheerio](https://cheerio.js.org/) parsing on each page
-  cheerio: true,
-  // If cheerio is activated do not forget to deactivate json parsing (which is activated by
-  // default in cozy-konnector-libs
+  cheerio: false,
   json: false,
-  // This allows request-promise to keep cookies between requests
   jar: true
 })
+const soap = require('soap')
 
-const VENDOR = 'template'
-const baseUrl = 'http://books.toscrape.com'
+const baseWSDL = 'https://www.silaexpert01.fr/Silae/SWS/SWS.asmx?WSDL'
+const baseUrl = 'https://www.silaexpert01.fr'
+const basePath = '/Silae/SWS/SWS.asmx'
+let repartiteurAdresse, token, id_domaine, id_paisalarie
 
 module.exports = new BaseKonnector(start)
 
-// The start function is run by the BaseKonnector instance only when it got all the account
-// information (fields). When you run this connector yourself in "standalone" mode or "dev" mode,
-// the account information come from ./konnector-dev-config.json file
-// cozyParameters are static parameters, independents from the account. Most often, it can be a
-// secret api key.
 async function start(fields, cozyParameters) {
   log('info', 'Authenticating ...')
   if (cozyParameters) log('debug', 'Found COZY_PARAMETERS')
-  await authenticate(fields.login, fields.password)
+  const client = await authenticate(fields.login, fields.password)
   log('info', 'Successfully logged in')
-  // The BaseKonnector instance expects a Promise as return of the function
+  // The BaseKonnector instance expects a  Promise as return of the function
   log('info', 'Fetching the list of documents')
-  const $ = await request(`${baseUrl}/index.html`)
+  const $ = await request(`${baseUrl}/index.html`, client)
   // cheerio (https://cheerio.js.org/) uses the same api as jQuery (http://jquery.com/)
   log('info', 'Parsing list of documents')
   const documents = await parseDocuments($)
@@ -53,32 +43,73 @@ async function start(fields, cozyParameters) {
   })
 }
 
-// This shows authentication using the [signin function](https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#module_signin)
-// even if this in another domain here, but it works as an example
 function authenticate(username, password) {
-  return signin({
-    url: `http://quotes.toscrape.com/login`,
-    formSelector: 'form',
-    formData: { username, password },
-    // The validate function will check if the login request was a success. Every website has a
-    // different way to respond: HTTP status code, error message in HTML ($), HTTP redirection
-    // (fullResponse.request.uri.href)...
-    validate: (statusCode, $, fullResponse) => {
-      log(
-        'debug',
-        fullResponse.request.uri.href,
-        'not used here but should be useful for other connectors'
+  return new Promise((resolve, reject) =>
+    soap.createClient(baseWSDL, function (err, client) {
+      log('info', 'Client successfully created')
+      client.SWS_SiteLoginEx({
+        SWSLogin: '',
+        SWSPassword: '',
+        USRLogin: username,
+        USRPassword: password
+      },
+        function (err, result) {
+          resolve(result)
+        }
       )
-      // The login in toscrape.com always works except when no password is set
-      if ($(`a[href='/logout']`).length === 1) {
-        return true
-      } else {
-        // cozy-konnector-libs has its own logging function which format these logs with colors in
-        // standalone and dev mode and as JSON in production mode
-        log('error', $('.error').text())
-        return false
-      }
-    }
+    })
+  ).then(result => {
+    const adresseFermeDistante =
+      result.SWS_SiteLoginExResult.AdresseFermeDistante
+    log(
+      'info',
+      'Using AdresseFermeDistante ' +
+      adresseFermeDistante
+    )
+    return new Promise((resolve, reject) =>
+      soap.createClient(baseWSDL, function (err, client) {
+        client.setEndpoint('https://' + adresseFermeDistante + basePath)
+        client.SWS_SiteLoginEx({
+          SWSLogin: '',
+          SWSPassword: '',
+          USRLogin: username,
+          USRPassword: password
+        },
+          function (err, result) {
+            resolve(result)
+          }
+        )
+      })
+    )
+  }).then(result => {
+      repartiteurAdresse = result.SWS_SiteLoginExResult.RepartiteurAdresse
+    log(
+      'info',
+      'Using RepartiteurAdresse ' +
+      repartiteurAdresse
+    )
+    return new Promise((resolve, reject) =>
+      soap.createClient(baseWSDL, function (err, client) {
+        client.setEndpoint('https://' + repartiteurAdresse + basePath)
+        client.SWS_SiteLoginEx({
+          SWSLogin: '',
+          SWSPassword: '',
+          USRLogin: username,
+          USRPassword: password
+        },
+          function (err, result) {
+            resolve(result)
+          }
+        )
+      })
+    )
+  }).then(result => {
+    console.log(result.SWS_SiteLoginExResult)
+    console.log(result.SWS_SiteLoginExResult.ListeOnglets.SWS_InformationsOnglet)
+    token = result.SWS_SiteLoginExResult.Token
+    id_domaine = result.SWS_SiteLoginExResult.ListeOnglets.SWS_InformationsOnglet[0].ID_DOMAINE
+    id_paisalarie = result.SWS_SiteLoginExResult.ListeOnglets.SWS_InformationsOnglet[0].ID_PAISALARIE
+    console.log(token, id_domaine, id_paisalarie)
   })
 }
 
@@ -89,8 +120,7 @@ function parseDocuments($) {
   // You can find documentation about the scrape function here:
   // https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#scrape
   const docs = scrape(
-    $,
-    {
+    $, {
       title: {
         sel: 'h3 a',
         attr: 'title'
@@ -111,7 +141,7 @@ function parseDocuments($) {
     ...doc,
     // The saveBills function needs a date field
     // even if it is a little artificial here (these are not real bills)
-    date: new Date(),
+    date: !new Date(),
     currency: 'EUR',
     filename: `${utils.formatDate(new Date())}_${VENDOR}_${doc.amount.toFixed(
       2
