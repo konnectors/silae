cc55522dd51acbbe8c00be998dab16a045aad42f
AZn5ReD <AZn5ReD@users.noreply.github.com>
2019-09-23 23:44:43 +0200

    Change to async + added sourceAccount

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable require-atomic-updates */
 const { BaseKonnector, saveFiles, log } = require('cozy-konnector-libs')
 const soap = require('soap')
 
@@ -20,123 +21,128 @@ async function start(fields, cozyParameters) {
   await savingDocuments(documentList, loginInfo, fields)
 }
 
-function authenticate(username, password) {
+async function authenticate(username, password) {
   let loginInfo = {}
   log('debug', 'Getting WSDL from ' + baseWSDL)
-  return new Promise((resolve, reject) =>
-    soap.createClient(baseWSDL, function(err, client) {
-      if (err) return reject(err)
-      log('info', 'Client successfully created')
-      client.SWS_SiteLoginEx(
-        {
-          SWSLogin: '',
-          SWSPassword: '',
-          USRLogin: username,
-          USRPassword: password
-        },
-        function(err, result) {
-          if (err) return reject(err)
-          if (result.SWS_SiteLoginExResult.Error)
-            reject(result.SWS_SiteLoginExResult.Error)
-          resolve(result)
-        }
-      )
-    })
-  )
-    .then(result => {
-      const adresseFermeDistante =
-        result.SWS_SiteLoginExResult.AdresseFermeDistante
-      log('debug', 'Using AdresseFermeDistante ' + adresseFermeDistante)
-      return new Promise((resolve, reject) =>
-        soap.createClient(baseWSDL, (err, client) => {
-          if (err) return reject(err)
-          client.setEndpoint('https://' + adresseFermeDistante + basePath)
-          client.SWS_SiteLoginEx(
-            {
-              SWSLogin: '',
-              SWSPassword: '',
-              USRLogin: username,
-              USRPassword: password
-            },
-            function(err, result) {
-              if (err) return reject(err)
-              if (result.SWS_SiteLoginExResult.Error)
-                reject(result.SWS_SiteLoginExResult.Error)
-              resolve(result)
-            }
-          )
-        })
-      )
-    })
-    .then(result => {
-      loginInfo.repartiteurAdresse =
-        result.SWS_SiteLoginExResult.RepartiteurAdresse
-      log('debug', 'Using RepartiteurAdresse ' + loginInfo.repartiteurAdresse)
-      return new Promise((resolve, reject) =>
-        soap.createClient(baseWSDL, (err, client) => {
-          if (err) return reject(err)
-          client.setEndpoint(
-            'https://' + loginInfo.repartiteurAdresse + basePath
-          )
-          client.SWS_SiteLoginEx(
-            {
-              SWSLogin: '',
-              SWSPassword: '',
-              USRLogin: username,
-              USRPassword: password
-            },
-            function(err, result) {
-              if (err) return reject(err)
-              if (result.SWS_SiteLoginExResult.Error)
-                reject(result.SWS_SiteLoginExResult.Error)
-              resolve(result)
-            }
-          )
-        })
-      )
-    })
-    .then(result => {
-      loginInfo.token = result.SWS_SiteLoginExResult.Token
-      loginInfo.id_domaine =
-        result.SWS_SiteLoginExResult.ListeOnglets.SWS_InformationsOnglet[0].ID_DOMAINE
-      loginInfo.id_paisalarie =
-        result.SWS_SiteLoginExResult.ListeOnglets.SWS_InformationsOnglet[0].ID_PAISALARIE
-      log('debug', 'Token ' + loginInfo.token)
-      log('debug', 'ID_DOMAINE ' + loginInfo.id_domaine)
-      log('debug', 'ID_PAISALARIE ' + loginInfo.id_paisalarie)
-      return loginInfo
-    })
-    .catch(error => log('error', error))
+
+  try {
+    const siteLoginClient = await new Promise((resolve, reject) =>
+      soap.createClient(baseWSDL, function(err, client) {
+        if (err) return reject(err)
+        log('info', 'Client successfully created')
+        client.SWS_SiteLoginEx(
+          {
+            SWSLogin: '',
+            SWSPassword: '',
+            USRLogin: username,
+            USRPassword: password
+          },
+          function(err, result) {
+            if (err) return reject(err)
+            if (result.SWS_SiteLoginExResult.Error)
+              reject(result.SWS_SiteLoginExResult.Error)
+            resolve(result)
+          }
+        )
+      })
+    )
+
+    const adresseFermeDistante =
+      siteLoginClient.SWS_SiteLoginExResult.AdresseFermeDistante
+    log('debug', 'Using AdresseFermeDistante ' + adresseFermeDistante)
+
+    const adresseFermeDistanteClient = await new Promise((resolve, reject) =>
+      soap.createClient(baseWSDL, (err, client) => {
+        if (err) return reject(err)
+        client.setEndpoint('https://' + adresseFermeDistante + basePath)
+        client.SWS_SiteLoginEx(
+          {
+            SWSLogin: '',
+            SWSPassword: '',
+            USRLogin: username,
+            USRPassword: password
+          },
+          function(err, result) {
+            if (err) return reject(err)
+            if (result.SWS_SiteLoginExResult.Error)
+              reject(result.SWS_SiteLoginExResult.Error)
+            resolve(result)
+          }
+        )
+      })
+    )
+
+    loginInfo.repartiteurAdresse =
+      adresseFermeDistanteClient.SWS_SiteLoginExResult.RepartiteurAdresse
+    log('debug', 'Using RepartiteurAdresse ' + loginInfo.repartiteurAdresse)
+
+    const repartiteurAdresseClient = await new Promise((resolve, reject) =>
+      soap.createClient(baseWSDL, (err, client) => {
+        if (err) return reject(err)
+        client.setEndpoint('https://' + loginInfo.repartiteurAdresse + basePath)
+        client.SWS_SiteLoginEx(
+          {
+            SWSLogin: '',
+            SWSPassword: '',
+            USRLogin: username,
+            USRPassword: password
+          },
+          function(err, result) {
+            if (err) return reject(err)
+            if (result.SWS_SiteLoginExResult.Error)
+              reject(result.SWS_SiteLoginExResult.Error)
+            resolve(result)
+          }
+        )
+      })
+    )
+
+    loginInfo.token = await repartiteurAdresseClient.SWS_SiteLoginExResult.Token
+    loginInfo.id_domaine = await repartiteurAdresseClient.SWS_SiteLoginExResult
+      .ListeOnglets.SWS_InformationsOnglet[0].ID_DOMAINE
+    loginInfo.id_paisalarie = await repartiteurAdresseClient
+      .SWS_SiteLoginExResult.ListeOnglets.SWS_InformationsOnglet[0]
+      .ID_PAISALARIE
+
+    log('debug', 'Token ' + loginInfo.token)
+    log('debug', 'ID_DOMAINE ' + loginInfo.id_domaine)
+    log('debug', 'ID_PAISALARIE ' + loginInfo.id_paisalarie)
+  } catch (error) {
+    log('error', error)
+  }
+  return loginInfo
 }
 
-function getDocumentList(loginInfo) {
-  return new Promise((resolve, reject) =>
-    soap.createClient(baseWSDL, function(err, client) {
-      if (err) return reject(err)
-      client.setEndpoint('https://' + loginInfo.repartiteurAdresse + basePath)
-      client.SWS_UtilisateurSalarieListeBulletins(
-        {
-          Token: loginInfo.token,
-          ID_DOMAINE: loginInfo.id_domaine,
-          ID_PAISALARIE: loginInfo.id_paisalarie
-        },
-        function(err, result) {
-          if (err) return reject(err)
-          if (result.SWS_UtilisateurSalarieListeBulletinsResult.Error)
-            reject(result.SWS_UtilisateurSalarieListeBulletinsResult.Error)
-          resolve(
-            result.SWS_UtilisateurSalarieListeBulletinsResult.Elements
-              .CPAISWSUtilisateurSalarieListeBulletinsElement
-          )
-        }
-      )
-    })
-  )
-    .then(result => {
-      log('debug', result)
-      return result
-    })
-    .catch(error => log('error', error))
+async function getDocumentList(loginInfo) {
+  let documentList
+  try {
+    documentList = await new Promise((resolve, reject) =>
+      soap.createClient(baseWSDL, function(err, client) {
+        if (err) return reject(err)
+        client.setEndpoint('https://' + loginInfo.repartiteurAdresse + basePath)
+        client.SWS_UtilisateurSalarieListeBulletins(
+          {
+            Token: loginInfo.token,
+            ID_DOMAINE: loginInfo.id_domaine,
+            ID_PAISALARIE: loginInfo.id_paisalarie
+          },
+          function(err, result) {
+            if (err) return reject(err)
+            if (result.SWS_UtilisateurSalarieListeBulletinsResult.Error)
+              reject(result.SWS_UtilisateurSalarieListeBulletinsResult.Error)
+            resolve(
+              result.SWS_UtilisateurSalarieListeBulletinsResult.Elements
+                .CPAISWSUtilisateurSalarieListeBulletinsElement
+            )
+          }
+        )
+      })
+    )
+    await log('debug', documentList)
+  } catch (error) {
+    log('error', error)
+  }
+  return documentList
 }
 
 function savingDocuments(documentList, loginInfo, fields) {
@@ -180,7 +186,11 @@ function savingDocuments(documentList, loginInfo, fields) {
                     contentType: 'application/pdf'
                   }
                 ],
-                fields
+                fields,
+                {
+                  sourceAccount: this._account._id,
+                  sourceAccountIdentifier: fields.login
+                }
               )
               resolve(result)
             }
